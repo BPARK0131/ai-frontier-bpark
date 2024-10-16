bf2c836051878cad651dea34be035bffaa54fbe6
BPARK0131 <bh0131.park@gmail.com>
2024-10-16 07:46:50 +0000

    TEST

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My app_TEST_BPARK")
+st.title("🎈 한글버전입니다 V2 .")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
